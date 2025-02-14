d209d66a3372661c4cc5983d341f940fbe4e8c57
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-02-14 20:49:25 +0400

    feat: add zod for form validation in form component

--- LoginFormWithValidation/src/components/Form.jsx
+++ LoginFormWithValidation/src/components/Form.jsx
@@ -1,65 +1,74 @@
-import { useRef } from "react";
-import "../css/Form.css"
 import {useForm} from "react-hook-form";
-function Form() {
-  const {register, handleSubmit,formState: {errors,isSubmitting}} = useForm();
-  const passwordField = useRef(null);
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 
-  const onSubmit =  (data)=>{
-    console.log(data)
+import "../css/Form.css"
+
+
+
+const userSchema = z.object({
+  userName: z.string().min(1,{
+    message: "name is required"
+  }),
+  email: z.string().email({
+    message: "email is not valid"
+  }),
+  password1: z.string().min(8,{
+    message: "password must be at least 8 characters long"
+  }),
+  password2: z.string({
+    message: "password is required"
+  }).min(8,{
+    message: "password must be at least 8 characters long"
+  }),
+}).superRefine(({ password2, password1 }, ctx) => {
+  if (password2 !== password1) {
+    ctx.addIssue({
+      code: "custom",
+      message: "passwords must match",
+      path: ['password2']
+    });
   }
+});
+
+function Form() {
+  const {register, handleSubmit,formState: {errors,isSubmitting}} = useForm({
+    defaultValues:{
+      userName:"John Doe",
+      email: "example@gmail.com"
+    },
+    resolver: zodResolver(userSchema)
+  });
+
+  const onSubmit = (data)=>{
+    console.log(data);
+  }
+
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
 
       <div className="content-wrapper">
-        <label htmlFor="name">User Name</label>
-        <input {...register("name",{
-          required: "name is required",
-          minLength:5
-        })} type="text" placeholder="john doe" />
-        {errors.name && <div className="error">{errors.name.message} </div>}
+        <label htmlFor="userName">User Name</label>
+        <input {...register("userName")} type="text" placeholder="john doe" />
+        {errors.userName && <div className="error">{errors.userName.message} </div>}
       </div>
       
       <div className="content-wrapper">
         <label htmlFor="email">Email</label>
-        <input  {...register("email",{
-          required: "email is required",
-          validate: (str) => {
-            if(!str.includes("@")){
-              return "email must include @ symbol"
-            }
-            return true;
-          }
-        })} type="email" placeholder="johndoe@gmail.com" />
+        <input  {...register("email")} type="email" placeholder="johndoe@gmail.com" />
         {errors.email && <div className="error">{errors.email.message} </div>}
       </div>
 
       <div className="content-wrapper">
         <label htmlFor="password1">Password</label>
-        <input  {...register("password1",{
-          required: "password is required",
-          minLength: {
-            value: 8,
-            message: "password must have at least 8 characters"
-          }
-        })} type="password" ref={passwordField} />
+        <input  {...register("password1")} type="password" />
         {errors.password1 && <div className="error">{errors.password1?.message} </div>}
       </div>
 
       <div className="content-wrapper">
         <label htmlFor="password2">Check password</label>
-        <input  {...register("password2",{
-          required: "password is required",
-          validate: (passw)=>{
-            if(passw != passwordField.current.value){
-              return "passwords must match"
-            }
-          },
-          minLength: {
-            value: 8,
-            message: "password must have at least 8 characters"
-          }
-        })} type="password"/>
+        <input  {...register("password2")} type="password"/>
         {errors.password2 && <div className="error">{errors.password2.message} </div>}
       </div>
 
